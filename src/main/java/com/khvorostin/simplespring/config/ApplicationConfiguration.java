311e80929fd1c92cd8c258ca76e063c9c5fc9a68
Denis Khvorostin <denis.khvorostin@gmail.com>
2022-05-30 18:14:13 +0300

    #hw2. Консольное приложение, позволяющее управлять корзиной

---
+++ src/main/java/com/khvorostin/simplespring/config/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package com.khvorostin.simplespring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.khvorostin.simplespring")
+public class ApplicationConfiguration {
+}
