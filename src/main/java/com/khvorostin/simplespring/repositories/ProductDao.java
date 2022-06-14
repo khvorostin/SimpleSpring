a4242e3d8abcf5435c3ed5c0c37a154ce2cdcef2
Denis Khvorostin <denis.khvorostin@gmail.com>
2022-06-15 02:03:55 +0300

    #hw5. ProductDao

--- src/main/java/com/khvorostin/simplespring/repositories/ProductDao.java
+++ src/main/java/com/khvorostin/simplespring/repositories/ProductDao.java
@@ -1,57 +1,65 @@
 package com.khvorostin.simplespring.repositories;
 
 import com.khvorostin.simplespring.models.Product;
-import com.khvorostin.simplespring.utils.SessionUtil;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import java.util.*;
 
-@Repository
+@Component
 public class ProductDao {
 
-    private SessionUtil sessionUtil;
+    private SessionFactory sessionFactory;
 
-    @Autowired
-    public ProductDao(SessionUtil sessionUtil) {
-        this.sessionUtil = sessionUtil;
+    public ProductDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
     public Product findById(Long id) {
-        sessionUtil.openSession();
-        String sql = "SELECT id, title, price FROM products WHERE id = :id";
-        Session session = sessionUtil.getSession();
-        Query< Product > query = session.createNativeQuery(sql, Product.class);
-        query.setParameter("id", id);
-        Product product = query.getSingleResult();
-        sessionUtil.closeSession();
+        Product product;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Transaction transaction = session.beginTransaction();
+            String sql = "SELECT id, title, price FROM products WHERE id = :id";
+            Query< Product > query = session.createNativeQuery(sql, Product.class);
+            query.setParameter("id", id);
+            product = query.getSingleResult();
+            session.getTransaction().commit();
+        }
         return product;
     }
 
     public List<Product> findAll() {
-        sessionUtil.openSession();
-        String sql = "SELECT id, title, price FROM products ORDER BY id LIMIT 10"; // чтобы не тащить всё, если будет 100500 записей
-        Session session = sessionUtil.getSession();
-        Query< Product > query = session.createNativeQuery(sql, Product.class);
-        List<Product> products = query.list();
-        sessionUtil.closeSession();
+        List<Product> products;
+        try (Session session = sessionFactory.getCurrentSession()){
+            session.beginTransaction();
+            String sql = "SELECT id, title, price FROM products ORDER BY id LIMIT 10"; // чтобы не тащить всё, если будет 100500 записей
+            Query< Product > query = session.createNativeQuery(sql, Product.class);
+            products = query.list();
+            session.getTransaction().commit();
+        }
         return products;
     }
 
     public Product saveOrUpdate(Product product) {
-        Session session = sessionUtil.openTransactionSession();
-        session.persist(product);
-        session.flush();
-        sessionUtil.closeTransactionSession();
+        try (Session session = sessionFactory.getCurrentSession()){
+            session.beginTransaction();
+            session.persist(product);
+            session.flush();
+            session.getTransaction().commit();
+        }
         return product;
     }
 
     public void deleteById(Long id) {
-        Session session = sessionUtil.openTransactionSession();
-        session.remove(findById(id));
-        session.flush();
-        sessionUtil.closeTransactionSession();
+        try (Session session = sessionFactory.getCurrentSession()){
+            session.beginTransaction();
+            session.remove(findById(id));
+            session.flush();
+            session.getTransaction().commit();
+        }
     }
 }
